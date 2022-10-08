b76fd0de82a9ee50b344e14ee53a417d37aea5a3
Ansh <anshsaxena4190@gmail.com>
2022-10-08 17:26:38 +0530

    taking to github

---
+++ components/login.js
@@ -0,0 +1,8 @@
+// import { ethers } from 'ethers'
+// import web3modal from 'web3modal'
+
+// export function Login() {
+//     return(
+//         {}
+//     )
+// }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,74 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme 
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { infuraProvider } from 'wagmi/providers/infura'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  // ------------
+
+  const avalancheChain = {
+    id: 43_114,
+    name: 'Avalanche',
+    network: 'avalanche',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Avalanche',
+      symbol: 'AVAX',
+    },
+    rpcUrls: {
+      default: 'https://api.avax.network/ext/bc/C/rpc',
+    },
+    blockExplorers: {
+      default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    },
+    testnet: false,
+  }
+
+  const { chains, provider } = configureChains(
+    [avalancheChain],
+    [
+      infuraProvider({ apiKey: '1dbc3ef8703a4669a5cda4f7de7343bc'}),
+      jsonRpcProvider({
+        rpc: (chain) => {
+          if (chain.id !== avalancheChain.id) return null
+          return { http: chain.rpcUrls.default }
+        },
+      }),
+    ]
+  )
+
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  })
+
+  // ------------
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>
+      <Component {...pageProps} />
+    </RainbowKitProvider>
+  </WagmiConfig>
+  )
 }
 
 export default MyApp
